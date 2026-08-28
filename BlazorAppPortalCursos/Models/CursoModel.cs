7aed71ac98eddff426c0b908f28b9512957036f9
Henrique Lopes Borges de Souza <henriquelopesb@gmail.com>
2026-08-27 21:48:21 -0300

    criação do cursomodel e implementação

---
+++ BlazorAppPortalCursos/Models/CursoModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+
+namespace BlazorAppPortalCursos.Models
+{
+    public class CursoModel
+    {
+        public string NomeCurso { get; set; } = "Curso de Programação Web";
+        public int CargaHoraria { get; set; } = 30;
+        public string Resumo { get; set; } = "Curso de programação Web utilizando a tecnologia da microsoft Blazor Server";
+
+
+    }
+}
