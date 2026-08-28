a07829a531e873d62ddabecdba86655dfae05e83
Henrique Lopes Borges de Souza <henriquelopesb@gmail.com>
2026-08-27 21:09:26 -0300

    First commit

---
+++ BlazorAppPortalCursos/Models/AlunoModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorAppPortalCursos.Models
+{
+    public class AlunoModel
+    {
+        [Required(ErrorMessage = "Informe o nome.")]
+        public string Nome { get; set; } = string.Empty;
+
+        [Range(16, 100, ErrorMessage = "Idade entre 16 e 100.")]
+        public int Idade { get; set; } = 18;
+
+        [Required(ErrorMessage = "Informe a data de nascimento.")]
+        public DateTime DataNascimento { get; set; } = DateTime.Today;
+
+        [Required(ErrorMessage = "Selecione o turno.")]
+        public string Turno { get; set; } = string.Empty;
+
+        [Range(typeof(bool), "true", "true", ErrorMessage = "É necessário aceitar os termos.")]
+        public bool AceitaTermos { get; set; }
+    }
+}
---
+++ BlazorAppPortalCursos/Models/InscricaoModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorAppPortalCursos.Models
+{
+    public class InscricaoModel
+    {
+        [Required(ErrorMessage="O nome é obrigatório")]
+        [StringLength(80, ErrorMessage ="Máximo de 80 caracteres.")]
+        public string Nome { get; set; } = string.Empty;
+        [Required(ErrorMessage = "O e-mail é obrigatório")]
+        [EmailAddress(ErrorMessage ="Informe um e-mail válido.")]
+        public string Email {  get; set; } = string.Empty;
+        [Required(ErrorMessage = "Selecione um curso")]
+        public string Curso {  get; set; } = string.Empty;
+        [StringLength(300, ErrorMessage = "Máximo de 300 caracteres.")]
+        public string? Comentario { get; set; }
+    }
+}
